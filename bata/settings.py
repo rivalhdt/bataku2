6e6d8a2663b856fceeaa59447f63df3ba3881703
rivalhdt <108782560+rivalhdt@users.noreply.github.com>
2022-07-06 12:37:42 +0700

    Update settings.py

--- bata/settings.py
+++ bata/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jcobym+tz8#0^o_fmm9p52t8-co*$q#+l)_9nu%-o_)_7=&gl2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'bataku2.herokuapp.com/']
 
 
 # Application definition
